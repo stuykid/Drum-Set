9d7ed4bdc002eb1b3078039021379f0dd7163dae
sheriff legrand <sheriff@sheriffs-Air.lan>
2023-03-07 16:11:45 -0500

    Update with JQuery

--- index.js
+++ index.js
@@ -1,87 +1,111 @@
-            /* MAKE SOUND FOR DETECTING A BUTTON CLICK */
+                    /*....... REFACTORED WITH JQUERY.........*/
+                    
+                    /* DETECT BUTTON CLICK */
 
-let lenButtons = document.querySelectorAll(".drum").length; //length of buttons in array w/class of drum
-let eachButton = document.querySelectorAll(".drum"); // target each individual button
 
-for (let i = 0; i < lenButtons; i++){ //loop through buttons 1-7
+/* let numButtons = document.querySelectorAll(".drum").length;
+let drum = document.querySelectorAll(".drum");
 
-    eachButton[i].addEventListener("click", function (){ //assign event listener to each button
+for (let i = 0; i < numButtons; i++){
+    drum[i].addEventListener("click", function(){
+        let buttonInnerHTML = this.innerHTML;
+        handleSound(buttonInnerHTML);
+        addAnimation(buttonInnerHTML);
         
-        let buttonInnerHTML = this.innerHTML; //target the letter of the button clicked 
         
-        handleSound(buttonInnerHTML); //add sound to button clicked
+    })
+} */
 
-        buttonAnimation(buttonInnerHTML); //add animation to button clicked
-    }); 
 
-    
-    
+$(".drum").click(function(){              // assign click event listener to every button w/.drum class
+    let buttonInnerHTML = this.innerHTML; // target the letter of the button clicked 
+    handleSound(buttonInnerHTML);         // add sound to button clicked
+    addAnimation(buttonInnerHTML);        // add animation to button clicked
+})
+
+
+
+
+                    /* DETECT KEYBOARD PRESS */
+
+/* document.addEventListener("keydown", function(event){
+    handleSound(event.key);
+    addAnimation(event.key);
+}) */
+
+$(document).keydown(function(event){     // assign keypress event listener to entire webpage
+    handleSound(event.key);              // add sound to button clicked
+    addAnimation(event.key);             // add animation to button clicked
+})
+
+
+
+
+                    /* ANIMATION */
+
+/* function addAnimation(currentKey){
+    let activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed"); //add the css class "pressed" to button clicked/pressed
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    }, 100);
+}; */
+
+function addAnimation(currentKey){  // add pressed animation when clicked/pressed
+    $("." + currentKey).addClass("pressed"); // assign class "pressed" to button
+
+    setTimeout(function(){
+        $("." + currentKey).removeClass("pressed") // remove class "pressed" after .01 seconds
+    }, 100);
+
 };
 
 
-            /*Make sound for keyboard presses*/
 
-document.addEventListener("keydown", function(event){ //callback function to add listener to entire page for a keystroke
-    
-    handleSound(event.key); //add sound to button triggered by keystroke
+                    /* HANDLE SOUND */
 
-    buttonAnimation(event.key); //add animation to button triggered by keystroke
-});
-
-
-
-function handleSound(key){ //function to make sound
-    
-    switch (key) { // switch statement to play sound based on button clicked/ keypress
-        case 'w': 
+function handleSound(key){
+    switch (key) {
+        case 'w':
             let tom1 = new Audio("./sounds/tom-1.mp3");
             tom1.play();
             
             break;
-
-        case "a":
+        case 'a':
             let tom2 = new Audio("./sounds/tom-2.mp3");
             tom2.play();
+                
             break;
-
-        case "s":
+        case 's':
             let tom3 = new Audio("./sounds/tom-3.mp3");
             tom3.play();
+            
             break;
-
-        case "d":
+        case 'd':
             let tom4 = new Audio("./sounds/tom-4.mp3");
             tom4.play();
+                
             break;
-
-        case "j":
+        case 'j':
             let snare = new Audio("./sounds/snare.mp3");
             snare.play();
+                
             break;
-
-        case "k":
+        case 'k':
             let crash = new Audio("./sounds/crash.mp3");
             crash.play();
+                
             break;
-
-        case "l":
-            let bass = new Audio("./sounds/kick-bass.mp3");
-            bass.play();
+        case 'l':
+            let kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+                    
             break;
 
         default:
             console.log(this.innerHTML);
-            
+            break;
     }
-};
-
-function buttonAnimation(currentKey){ // add pressed animation when clicked/pressed
-    let activeButton = document.querySelector("." + currentKey); //("." + "letter") target the activebutton pressed
-
-    activeButton.classList.add("pressed"); // assign class "pressed" to button
-
-    setTimeout(function(){
-        activeButton.classList.remove("pressed"); // after 1/100 second, remove class "pressed"
-    }, 100 );
 
 }
